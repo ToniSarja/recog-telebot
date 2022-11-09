8c70f47c1c8b57b0a8f53eef52d5d7b0d131292b
ToniSarja <82703562+ToniSarja@users.noreply.github.com>
2022-11-09 11:32:34 +0200

    Update recog_botti.py

--- recog_botti.py
+++ recog_botti.py
@@ -24,9 +24,13 @@ def send_welcome(message):
 def echo_all(message):
 	bot.reply_to(message, message.text)
 
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Placeholder text")
 
 @bot.message_handler(content_types=['voice', 'audio'])
 def get_audio_messages(message):
+    #load the model
     model = keras.models.load_model('chat_model')
     max_len = 20
     with open('tokenizer.pickle', 'rb') as handle:
@@ -35,6 +39,7 @@ def get_audio_messages(message):
     with open('label_encoder.pickle', 'rb') as enc:
         lbl_encoder = pickle.load(enc)
 
+    #bot graps the audio file
     bot.reply_to(message, "Pieni hetki")
     r = sr.Recognizer()
     file_info = bot.get_file(message.voice.file_id)
@@ -43,14 +48,17 @@ def get_audio_messages(message):
         new_file.write(downloaded_file)
     src_filename = 'user_voice.ogg'
     dest_filename = 'user_voice_output.wav'
-
+    
+    #change of format
     sound = AudioSegment.from_ogg(src_filename)
     sound.export(dest_filename, format="wav")
 
     user_audio_file = sr.AudioFile("user_voice_output.wav")
     with user_audio_file as source:
+        #speech recognition happens here
         user_audio = r.record(source)
         text = r.recognize_google(user_audio, language='ru')
+        #classification and iterating thru the JSON dataset
         result = model.predict(keras.preprocessing.sequence.pad_sequences(tokenizer.texts_to_sequences([text]),truncating='post', maxlen=max_len))
         tag = lbl_encoder.inverse_transform([np.argmax(result)])
         for i in data['intents']:
